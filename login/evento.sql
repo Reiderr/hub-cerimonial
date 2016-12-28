6cd4fa416380c99175fb0226729f0b247483ff28
Reiderr <marcosreider@hotmail.com>
2016-12-28 16:17:05 -0200

    atualizado sql

--- login/evento.sql
+++ login/evento.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 27-Dez-2016 às 03:10
+-- Generation Time: 28-Dez-2016 às 19:15
 -- Versão do servidor: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -32,8 +32,24 @@ CREATE TABLE `evento` (
   `local` varchar(255) DEFAULT NULL,
   `listaPresente` varchar(255) DEFAULT NULL,
   `convidados` int(11) DEFAULT NULL,
-  `Usuario_CPF` varchar(11) NOT NULL
+  `url` varchar(255) NOT NULL,
+  `user_Email` varchar(255) DEFAULT NULL,
+  `local_Latitude` varchar(25) DEFAULT NULL,
+  `local_Longitude` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `evento`
+--
+
+INSERT INTO `evento` (`idEvento`, `nomeEvento`, `local`, `listaPresente`, `convidados`, `url`, `user_Email`, `local_Latitude`, `local_Longitude`) VALUES
+(4, 'Casamento JoÃ£o e Maria', 'Rua da silva', 'linkaqui', 45, 'joaoemaria', NULL, '-19.924152', '-43.997993'),
+(6, 'Teste 1', 'Teste1 endereco', 'Teste1link', 0, 'Teste1url', NULL, '-19.917373', '-43.938255'),
+(8, 'sdasd', 'fdfd', 'fdfdsfsd', 147, 'joaoemaria2', NULL, '-19.917858', '-44.030352'),
+(9, 'sada', 'asdad@!dasda', 'dfsdf', 65456, 'df', NULL, '-19.917858', '-44.030352'),
+(10, 'sdsdxc', 'akdsdhjkasdasd', '', 56456, '56474897498237492743', NULL, '-19.917858', '-44.030352'),
+(11, 'dasdzxc', '23124', '', 5435345, '54s6d54asd', NULL, '-19.917858', '-44.030352'),
+(12, 'teste2', 'teste2end', 'teste2link', 233, 'teste2url', NULL, '-19.910272', '-43.965120');
 
 -- --------------------------------------------------------
 
@@ -69,7 +85,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`CPF`, `nome`, `telefone`, `endereco`, `admin`, `senha`, `email`) VALUES
-('118527', 'Marcos', 'rua2', 'rua 4', 1, '83b4ef5ae4bb360c96628aecda974200', 'marcos@hotmail.com');
+('', '', '', '', 0, 'd41d8cd98f00b204e9800998ecf8427e', ''),
+('118527', 'Marcos', 'rua2', 'rua 4', 1, '83b4ef5ae4bb360c96628aecda974200', 'marcos@hotmail.com'),
+('dsdd', 'sad', 'ssa', 'ddd', 0, '718b6dd54c8d1d3ad19eb99cb12f13e2', 'dd@dddd');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +99,7 @@ INSERT INTO `usuario` (`CPF`, `nome`, `telefone`, `endereco`, `admin`, `senha`, 
 ALTER TABLE `evento`
   ADD PRIMARY KEY (`idEvento`),
   ADD UNIQUE KEY `idEvento_UNIQUE` (`idEvento`),
-  ADD KEY `fk_Evento_Usuário_idx` (`Usuario_CPF`);
+  ADD UNIQUE KEY `url` (`url`);
 
 --
 -- Indexes for table `tokens`
@@ -107,16 +125,10 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `evento`
 --
 ALTER TABLE `evento`
-  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- Constraints for dumped tables
 --
-
---
--- Limitadores para a tabela `evento`
---
-ALTER TABLE `evento`
-  ADD CONSTRAINT `fk_Evento_Usuário` FOREIGN KEY (`Usuario_CPF`) REFERENCES `usuario` (`CPF`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Limitadores para a tabela `tokens`
